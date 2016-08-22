553b7166f94195ddd7e7d51736787b86d936711a
Huyen Ha <hathehuyen@gmail.com>
2016-08-22 11:28:40 +0700

    Add common API get employee by email (/common/employee)

--- career/service/common_service.py
+++ career/service/common_service.py
@@ -66,27 +66,12 @@ class CommonService(osv.AbstractModel):
 
     @api.model
     def searchEmployee(self, email):
-        employees = self.env['res.partner'].search([('email', '=', email)])
-        employeeList = [{'id': e.id, 'name': e.name, 'email': e.email,
-                         'mobile': e.mobile or False,
-                         'countryId': e.country_id.id} for e in employees]
-        '''employeeList = [{'id': e.id, 'name': e.user_id.partner_id.name, 'email': e.user_id.partner_id.email,
-                         'mobile': e.user_id.partner_id.mobile or False,
-                         'countryId': e.user_id.partner_id.country_id.id} for e in employees]'''
-        '''
-        for assignment in self.env['hr.job'].browse(email):
-            if assignment.category_ids:
-                for cat_id in assignment.category_ids.ids:
-                    for exp in self.env['career.work_experience'].search([]):
-                        if cat_id in exp.cat_ids.ids:
-                            employee_id = exp.employee_id
-                            candidate = {'id': employee_id.id, 'name': employee_id.user_id.name,
-                                         'email': employee_id.user_id.login}
-                            candidate['profile'] = employee_id.getProfile()
-                            candidate['expList'] = employee_id.getWorkExperience()
-                            candidate['eduList'] = employee_id.getEducationHistory()
-                            candidate['certList'] = employee_id.getCertificate()
-                            candidate['docList'] = employee_id.getDocument()
-                            candidateList.append(candidate)
-        '''
+        employees = self.env['career.employee'].search([('login', '=', email)])
+        employeeList = [{'id': e.id, 'name': e.user_id.name, 'email': e.user_id.login,
+                         'profile': e.getProfile(),
+                         'expList': e.getWorkExperience(),
+                         'eduList': e.getEducationHistory(),
+                         'certList': e.getCertificate(),
+                         'docList': e.getDocument(),
+                         } for e in employees]
         return employeeList
