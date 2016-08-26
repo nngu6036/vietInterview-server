6e11c768f9a0c8609b108fe3a5ada0aeaacd0830
Huyen Ha <hathehuyen@gmail.com>
2016-08-26 14:40:20 +0700

    Reformat file

--- career/service/license_service.py
+++ career/service/license_service.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from openerp import models, fields, api,tools
+from openerp import models, fields, api, tools
 from openerp.osv import osv
 from openerp.service import common
 import base64
@@ -13,69 +13,71 @@ from datetime import date, timedelta
 
 
 class LicenseService(osv.AbstractModel):
-	_name = 'career.license_service'
+    _name = 'career.license_service'
 
-	@api.model
-	def validateLicense(self,companyId):
-		for company in self.env['res.company'].browse(companyId):
-			if not company.license_instance_id.isEnabled():
-				return False
-			else:
-				return True
-		return False
+    @api.model
+    def validateLicense(self, companyId):
+        for company in self.env['res.company'].browse(companyId):
+            if not company.license_instance_id.isEnabled():
+                return False
+            else:
+                return True
+        return False
 
-	@api.model
-	def activateLicense(self,companyId):
-		for company in self.env['res.company'].browse(companyId):
-			if company.license_instance_id and company.license_instance_id.expire_date:
-				expire_date = datetime.datetime.strptime(company.license_instance_id.expire_date, "%Y-%m-%d")
-				if company.license_instance_id.state != 'active' and expire_date < datetime.datetime.now():
-					return False
-				company.license_instance_id.write({'state':'active'})
-				return True
-		return False
+    @api.model
+    def activateLicense(self, companyId):
+        for company in self.env['res.company'].browse(companyId):
+            if company.license_instance_id and company.license_instance_id.expire_date:
+                expire_date = datetime.datetime.strptime(company.license_instance_id.expire_date, "%Y-%m-%d")
+                if company.license_instance_id.state != 'active' and expire_date < datetime.datetime.now():
+                    return False
+                company.license_instance_id.write({'state': 'active'})
+                return True
+        return False
 
-	@api.model
-	def deactivateLicense(self,companyId):
-		for company in self.env['res.company'].browse(companyId):
-			if company.license_instance_id:
-				company.license_instance_id.write({'state':'suspend'})
-				return True
-		return False
+    @api.model
+    def deactivateLicense(self, companyId):
+        for company in self.env['res.company'].browse(companyId):
+            if company.license_instance_id:
+                company.license_instance_id.write({'state': 'suspend'})
+                return True
+        return False
 
-	@api.model
-	def consumeEmail(self,applicantId):
-		self.env['hr.applicant'].browse(applicantId)
-		for applicant in self.env['hr.applicant'].browse(applicantId):
-			license_instance = applicant.company_id.license_instance_id
-			if license_instance:
-				self.env['career.email.history'].create({'applicant_id':applicantId,'date_send':  fields.Datetime.now(),
-														'license_instance_id': license_instance.id})
-				email_quota = self.env['career.email.history'].search_count([('license_instance_id','=',license_instance.id)])
-				if email_quota >license_instance.license_id.email :
-					self.deactivateLicense(applicant.company_id.id)
-		return True
+    @api.model
+    def consumeEmail(self, applicantId):
+        self.env['hr.applicant'].browse(applicantId)
+        for applicant in self.env['hr.applicant'].browse(applicantId):
+            license_instance = applicant.company_id.license_instance_id
+            if license_instance:
+                self.env['career.email.history'].create(
+                    {'applicant_id': applicantId, 'date_send': fields.Datetime.now(),
+                     'license_instance_id': license_instance.id})
+                email_quota = self.env['career.email.history'].search_count(
+                    [('license_instance_id', '=', license_instance.id)])
+                if email_quota > license_instance.license_id.email:
+                    self.deactivateLicense(applicant.company_id.id)
+        return True
 
-	@api.model
-	def consumeEmployee(self, companyId,employeeId):
-		for company in self.env['res.company'].browse(companyId):
-			license_instance = company.license_instance_id
-			if license_instance:
-				self.env['career.employee.history'].create(
-					{'employee_id': employeeId,
-					 'license_instance_id': license_instance.id})
-				employee_quota = self.env['career.employee.history'].search_count([('license_instance_id', '=', license_instance.id)])
-				if employee_quota > license_instance.license_id.employee:
-					self.deactivateLicense(companyId)
-		return True
+    @api.model
+    def consumeEmployee(self, companyId, employeeId):
+        for company in self.env['res.company'].browse(companyId):
+            license_instance = company.license_instance_id
+            if license_instance:
+                self.env['career.employee.history'].create(
+                    {'employee_id': employeeId,
+                     'license_instance_id': license_instance.id})
+                employee_quota = self.env['career.employee.history'].search_count(
+                    [('license_instance_id', '=', license_instance.id)])
+                if employee_quota > license_instance.license_id.employee:
+                    self.deactivateLicense(companyId)
+        return True
 
-
-	@api.one
-	def renewLicense(self, companyId,licenseId):
-		license = self.env['career.license'].browse(int(licenseId))
-		expiryDdate = date.today() + timedelta(days=license.validity)
-		license_instance = self.env['career.license_instance'].create({'license_id': license.id,
-																	   'expire_date': '%d-%d-%d ' % (
-																		   expiryDdate.year, expiryDdate.month,
-																		   expiryDdate.day)})
-		return self.env['res.company'].browse(companyId).write({'license_instance_id': license_instance.id,})
+    @api.one
+    def renewLicense(self, companyId, licenseId):
+        license = self.env['career.license'].browse(int(licenseId))
+        expiryDdate = date.today() + timedelta(days=license.validity)
+        license_instance = self.env['career.license_instance'].create({'license_id': license.id,
+                                                                       'expire_date': '%d-%d-%d ' % (
+                                                                           expiryDdate.year, expiryDdate.month,
+                                                                           expiryDdate.day)})
+        return self.env['res.company'].browse(companyId).write({'license_instance_id': license_instance.id,})
