2d727b516c09f3fca7e01a16b1e0ca34f18dd307
Huyen Ha <hathehuyen@gmail.com>
2016-09-12 16:25:18 +0700

    Edit res_group data, add updateAdmin and getAdmins

--- career/model/admin.py
+++ career/model/admin.py
@@ -24,3 +24,24 @@ class Admin(models.Model):
                  'email': login, 'groups_id': [(6, 0, [admin_group.id, hr_group.id, survey_group.id, man_group.id])]})
             return user.id
         return False
+
+    @api.model
+    def updateAdmin(self, login, password, role):
+        users = self.env['res.users'].search([('login', '=', login)])
+        for user in users:
+            if user.write({'password': password}):
+                return True
+        return False
+
+    @api.model
+    def getAdmins(self):
+        adminList = []
+        users = self.env['res.users'].search()
+        admin_group = self.env.ref('career.admin_group')
+        cc_group = self.env.ref('career.cc_group')
+        for user in users:
+            if admin_group.id in user.groups_id.ids or cc_group.id in user.groups_id.ids:
+                admin['login'] = user.login
+                admin['role'] = user.groups_id.name
+                return True
+        return False
