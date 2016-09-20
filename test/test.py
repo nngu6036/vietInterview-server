2133e62408ad1a6e212a6e483bea4ead06d2a1e7
Quang Nguyen <quang.nguyennhat@vcc.vn>
2016-09-21 05:20:26 +0700

    Fix URL in mail

--- test/test.py
+++ test/test.py
@@ -4,11 +4,11 @@ from data import  *
 client = erppeek.Client('http://demo.vietinterview.com:8069', 'career', 'admin', '123456')
 #client = erppeek.Client('http://10.158.7.18:8069', 'career', 'admin', '123456')
 #user = client.model('res.users').browse([('login','=','hdsaison')])
-#user.company_id.write({'url':'https://hdsaison.vietinterview.com'})
-user = client.model('res.users').browse([('login','=','homecredit')])
-user.company_id.write({'url':'https://homecredit.demo.vietinterview.com'})
+
 #print user.company_id.url
-#job = client.model('hr.job').browse([])
+for job in  client.model('hr.job').browse([]):
+	print job.isEnabled()
+
 #job.write({'status':'published'})
 
 
