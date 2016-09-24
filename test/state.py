23f832c12b6390d5a0d40f4da9ae84cb6e8c2fdf
Quang Nguyen <quang.nguyennhat@vcc.vn>
2016-09-24 17:39:13 +0700

    Fix URL

--- test/state.py
+++ test/state.py
@@ -1,11 +1,9 @@
-pattern= "\<option value='(\d*)'\>(.+)\<\/option\>"
-file = open("E:/tmp/state", "r")
-i = 1
-for line in  file.readlines():
-    line = line.strip()
-    print "<record model='res.country.state' id='career.country_state_%d'>" %i
-    print "<field name='name'>%s</field>"%line
-    print "<field name='code'>%s</field>"%line[0:3].upper().strip()
-    print "<field name='country_id' ref='base.vn'/>"
-    print "</record>"
-    i = i+1
+from decimal import  *
+class Solution:
+   # input n: number of dice sides
+   def computeRatio(self,n):
+      assert n > 0
+      return reduce(lambda x,y:x*float(y+1)/n, range(n),1)
+
+a = Solution()
+print a.computeRatio(10)
