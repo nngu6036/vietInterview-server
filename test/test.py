700b38a75672e81701fc96f2dc24bacdbbdb6261
Huyen Ha <hathehuyen@gmail.com>
2016-09-27 10:35:19 +0700

    Remove default license_instance state that cause state change to initial when upgrade

--- test/test.py
+++ test/test.py
@@ -2,7 +2,7 @@ import erppeek
 from data import *
 
 client = erppeek.Client('http://demo.vietinterview.com:8069', 'career', 'admin', '123456')
-# client = erppeek.Client('http://10.158.7.18:8069', 'career', 'admin', '123456')
+#client = erppeek.Client('http://10.158.7.18:8069', 'career', 'admin', '123456')
 # user = client.model('res.users').browse([('login','=','hdsaison')])
 
 # print user.company_id.url
