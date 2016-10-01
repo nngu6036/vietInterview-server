43bca3c02c463826d4d404a86ad049e40eeeaeb5
Quang Nguyen <quang.nguyennhat@vcc.vn>
2016-10-01 12:06:42 +0700

    Fix URL in mail

--- test/state.py
+++
@@ -1,9 +0,0 @@
-from decimal import  *
-class Solution:
-   # input n: number of dice sides
-   def computeRatio(self,n):
-      assert n > 0
-      return reduce(lambda x,y:x*float(y+1)/n, range(n),1)
-
-a = Solution()
-print a.computeRatio(10)
