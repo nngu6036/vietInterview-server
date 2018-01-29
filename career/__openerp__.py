f7277ed4c5f2915a3bc880a332389c17722cd4f4
Quang Nguyen <technicalmanager@gmail.com>
2018-01-29 13:09:04 +1100

    Update __openerp__.py

--- career/__openerp__.py
+++ career/__openerp__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','hr','survey','hr_recruitment','email_template','mass_mailing','hr_evaluation'],
+    'depends': ['base','hr','survey','hr_recruitment','mail','mass_mailing','hr_evaluation'],
 
     # always loaded
     'data': [
